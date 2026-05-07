6748177165f8d594e148631d9b2356949f436c99
Vcout1nho <victorsamuelcoutinho125tu@gmail.com>
2026-05-07 14:39:55 -0300

    fix gmail ipv4

--- server.js
+++ server.js
@@ -13,13 +13,21 @@ const jwt = require("jsonwebtoken");
 const axios = require("axios");
 const app = express();
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
+    pass: process.env.EMAIL_PASS,
+  },
 
+  tls: {
+    rejectUnauthorized: false
+  },
+
+  family: 4
+});
 app.use(cors());
 app.use(express.json()); 
 
